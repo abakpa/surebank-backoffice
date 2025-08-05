24ff4081aea7876259c8e1b7e209606e271dff73
joseph abakpa <elachi4c@yahoo.com>
2025-08-06 00:56:16 +0100

    backend

--- src/redux/sagas/url.js
+++ src/redux/sagas/url.js
@@ -1,2 +1,3 @@
-export const url = 'https://surebank-backend.onrender.com'
+// export const url = 'https://surebank-backend.onrender.com'
+export const url = 'https://surebank-backend-wsbj.onrender.com'
 // export const url = 'http://localhost:4000'
