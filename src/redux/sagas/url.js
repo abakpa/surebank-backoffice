8d5b35e55d761c298f2cea91d2bc8a9e9d5dac6f
joseph abakpa <elachi4c@yahoo.com>
2025-08-12 14:30:08 +0100

    digital ocean backend url

--- src/redux/sagas/url.js
+++ src/redux/sagas/url.js
@@ -1,3 +1,4 @@
-// export const url = 'https://surebank-backend.onrender.com'
-export const url = 'https://surebank-backend-wsbj.onrender.com'
+// export const url = 'https://surebank-backend.onrender.com' //my github
+// export const url = 'https://surebank-backend-wsbj.onrender.com' //sure bank github
+export const url = 'https://backend-hz3lu.ondigitalocean.app' //sure bank github digital ocean
 // export const url = 'http://localhost:4000'
