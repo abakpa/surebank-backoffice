e6a3d57781ded7aee2bf8be1dc58770735f586ae
joseph abakpa <elachi4c@yahoo.com>
2025-09-22 11:15:26 +0100

    adding customer request for online rep

--- src/Components/ViewRepCustomer.js
+++ src/Components/ViewRepCustomer.js
@@ -134,13 +134,13 @@ const Viewcustomer = () => {
                   Transfer All Customers
                 </button>
         )}
-        {role === 'Agent' && (
+        {/* {role === 'Agent' && ( */}
             <Link to="/viewrepcustomerwithdrawalrequest" className="text-xs">
                                   <button className="w-full md:w-auto px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
                                     Withdrawal Request
                                   </button>
                                 </Link>
-        )}
+        {/* )} */}
         {role === 'Agent' && (
 
               <Link to="/viewrepcustomerusingapp" className="text-xs">
